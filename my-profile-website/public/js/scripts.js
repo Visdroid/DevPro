b34e6f957eb8fcf9c2e8393e9624385d295d2358
Visdroid <aldrinvisser@gmail.com>
2024-05-22 10:47:50 +0200

    6.Adding Client Side using Javascript

---
+++ my-profile-website/public/js/scripts.js
@@ -0,0 +1,28 @@
+document.getElementById('contact-form').addEventListener('submit',function(event){
+event.preventDefault();
+
+const name = document.getElementById('name').value;
+const email = document.getElementById('email').value;
+const message = document.getElementById('message').value;
+const phone = document.getElementById('phone').value; 
+
+fetch('/contact',{
+    method: 'POST',
+    headers:{
+        'Content-Type': 'application/json'
+    },
+})
+.then(Response => Response.json())
+.then(data => {
+    if(data.success){
+        alert('Message sent successfully!');
+    }else{
+        alert('Failed to send message.');
+    }
+})
+.catch(error =>{
+    console.error('Error:',error);
+    alert('An error occurred');
+});
+
+});
