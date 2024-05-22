ba290d2f7fed6124f978c1f08ddc2cdd07bbc549
Visdroid <aldrinvisser@gmail.com>
2024-05-22 11:46:29 +0200

    7.Adding Serside to conect to MongoDB

--- my-profile-website/public/js/scripts.js
+++ my-profile-website/public/js/scripts.js
@@ -1,28 +1,29 @@
-document.getElementById('contact-form').addEventListener('submit',function(event){
-event.preventDefault();
+document.getElementById('contact-form').addEventListener('submit', function (event) {
+    event.preventDefault();
 
-const name = document.getElementById('name').value;
-const email = document.getElementById('email').value;
-const message = document.getElementById('message').value;
-const phone = document.getElementById('phone').value; 
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+    const phone = document.getElementById('phone').value;
 
-fetch('/contact',{
-    method: 'POST',
-    headers:{
-        'Content-Type': 'application/json'
-    },
-})
-.then(Response => Response.json())
-.then(data => {
-    if(data.success){
-        alert('Message sent successfully!');
-    }else{
-        alert('Failed to send message.');
-    }
-})
-.catch(error =>{
-    console.error('Error:',error);
-    alert('An error occurred');
-});
+    fetch('/contact', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({name, email, message})
+    })
+        .then(Response => Response.json())
+        .then(data => {
+            if (data.success) {
+                alert('Message sent successfully!');
+            } else {
+                alert('Failed to send message.');
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            alert('An error occurred');
+        });
 
 });
---
+++ my-profile-website/server/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const contactRoute = require('../routes/contact');
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+//This is the Middleware
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+app.use('/contac',contactRoute);
+
+//Connecting to MongoDB
+mongoose.connect(process.env.MONGO_URL,{ useNewUrlParser: true, userUnifiedTopology: true})
+.then(() => console.log('MongoDB connected...'))
+.catch(err => console.log(err));
+
+app.listen(port, () => {
+    console.log('Server running on port ${port}');
+});
+
+
